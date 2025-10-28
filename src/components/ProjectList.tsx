89644f42d6273feb694085052d5899ac787580e4
indoorrockclimbing101 <juneplant2003@gmail.com>
2025-10-27 22:27:06 -1000

    Made ProjectList component

---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,71 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { Link } from "react-router-dom";
+import "./ProjectList.css";
+
+interface Project {
+  name: string;
+  rating: string;
+  link?: string;
+}
+
+interface Props {
+  type?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
+  projects: Project[];
+  onClick?: () => void;
+}
+
+const ProjectList = ({ type, projects, onClick }: Props) => {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <ul className="list-group overflow-auto list-design">
+          {projects.map((projects) => (
+            <li
+              className={
+                "text-dark list-group-item grid-design list-group-item-" + type
+              }
+            >
+              <div className="row">
+                {projects.link ? (
+                  <Link
+                    to={projects.link}
+                    className="text-decoration-none name col-md-auto"
+                  >
+                    <div>{projects.name}</div>
+                  </Link>
+                ) : (
+                  <div className="name col-md-auto">{projects.name}</div>
+                )}
+
+                <div
+                  className={
+                    projects.rating === "Low"
+                      ? "rating col-sm text-success"
+                      : projects.rating === "Medium"
+                      ? "rating col-sm text-warning"
+                      : projects.rating === "High"
+                      ? "rating col-sm text-danger"
+                      : "rating col-sm"
+                  }
+                >
+                  {projects.rating}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,20 +1,41 @@
 import Carousel from "../components/Carousel";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
-import "./Page.css";
+import "./Overview.css";
 import Metrics from "../components/Metrics";
+import ProjectList from "../components/ProjectList";
+
+const data = [
+  {
+    name: "Keiki Replatform Off Mainframe Project",
+    rating: "Low",
+    link: "./projects",
+  },
+  { name: "Business Registration Modernization", rating: "Medium" },
+  { name: "Integrated Case Management System", rating: "High" },
+  { name: "Med-Quest Health Analytics Program", rating: "Completed" },
+];
 
 //This page will follow the figma mockup of "Public: landing page displaying: project highlight cart, metrics & more"
 const Overview = () => {
   return (
-    <div>
+    <div className="top-pad">
+      <h1 className="header">Office of Enterprise Technology Services</h1>
+      <h6>ABOUT US</h6>
+      {/* Carousle */}
       <Carousel></Carousel>
+
+      {/* View all projects button */}
       <Link
         to="/projects"
         className="text-decoration-none d-flex justify-content-end"
       >
         <Button>VIEW ALL ETS PROJECTS</Button>
       </Link>
+
+      <h1 className="header">ETS PROJECT METRIC OVERVIEW</h1>
+
+      {/* Metrics */}
       <div className="container">
         <div className="row">
           <div className="col">
@@ -28,6 +49,19 @@ const Overview = () => {
           </div>
         </div>
       </div>
+
+      <h1 className="header">ETS PROJECT METRIC QUICK DASHBOARD</h1>
+
+      {/* List of projects */}
+      <ProjectList projects={data} type="light"></ProjectList>
+
+      {/* View all projects button */}
+      <Link
+        to="/projects"
+        className="text-decoration-none d-flex justify-content-end"
+      >
+        <Button>VIEW ALL ETS PROJECTS</Button>
+      </Link>
     </div>
   );
 };
