a1b42428cb4cf69d238ad4cdf0c9bd8271f8315b
indoorrockclimbing101 <juneplant2003@gmail.com>
2025-10-29 13:46:36 -1000

    Updated button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 import "./Button.css";
