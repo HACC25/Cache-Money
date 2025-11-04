d279f9399d6e79b93dfaade24de9b9945f54498f
Karina Park-Okuna <lokelanipark@gmail.com>
2025-11-04 12:15:04 -1000

    update sign in button

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -136,9 +136,10 @@ const SignIn = () => {
           <div>
             <button
               type="submit"
+              disabled={isLoading}
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              {isLoading ? "Signing in..." : "Sign in"}
             </button>
           </div>
         </form>
