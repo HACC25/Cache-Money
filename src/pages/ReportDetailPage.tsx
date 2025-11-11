58dbac763e8009c384a63c40a8086946610f8292
Karina Park-Okuna <lokelanipark@gmail.com>
2025-11-10 23:48:28 -1000

    Update Report Page

--- src/pages/ReportDetailPage.tsx
+++ src/pages/ReportDetailPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../services/firebase-config";
-import { fetchProjectById } from "../services/FirebaseDataService";
+import { fetchProjectById } from "../services/firebaseDataService";
 import type { ProjectData, ProjectReport } from "../components/SampleData";
 
 const ReportDetailPage: React.FC = () => {
